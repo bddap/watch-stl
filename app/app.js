3bfd6ae411f88cc770847b333f5a0dce9b1e610f
Andrew Dirksen <andrew@dirksen.com>
2016-10-23 15:53:02 -0700

    watch

--- app/app.js
+++ app/app.js
@@ -9,22 +9,29 @@ const stl = require('stl')
 
 let mesh = new three.Object3D()
 
-fs.readFile(nw.App.argv[0], (err, data) => {
-  err && console.error(err)
+function load(){
+  fs.readFile(nw.App.argv[0], (err, data) => {
+    err && console.error(err)
 
-  const facets = stl.toObject(data).facets
-  const triangles = facets.map(facet => facet.verts)
-  const normals = facets.map(facet => [facet.normal,facet.normal,facet.normal]) //make a normal for each vetex because MeshLambertMaterial uses normal in the vertex shader
+    if (mesh.geometry) {
+      scene.remove(mesh)
+      mesh.geometry.dispose()
+    }
 
-  const vertices = new Float32Array( flatten(flatten(triangles)) )
-  const normal = new Float32Array( flatten(flatten(normals)) )
-  const geometry = new three.BufferGeometry();
-  geometry.addAttribute( 'position', new three.BufferAttribute( vertices, 3 ) )
-  geometry.addAttribute( 'normal', new three.BufferAttribute( normal, 3 ) )
-  mesh = new three.Mesh( geometry, new three.MeshLambertMaterial( { color: 0xffffff } ) )
+    const facets = stl.toObject(data).facets
+    const triangles = facets.map(facet => facet.verts)
+    const normals = facets.map(facet => [facet.normal,facet.normal,facet.normal]) //make a normal for each vetex because MeshLambertMaterial uses normal in the vertex shader
 
-  scene.add(mesh)
-})
+    const vertices = new Float32Array( flatten(flatten(triangles)) )
+    const normal = new Float32Array( flatten(flatten(normals)) )
+    const geometry = new three.BufferGeometry();
+    geometry.addAttribute( 'position', new three.BufferAttribute( vertices, 3 ) )
+    geometry.addAttribute( 'normal', new three.BufferAttribute( normal, 3 ) )
+    mesh = new three.Mesh( geometry, new three.MeshLambertMaterial( { color: 0xffffff } ) )
+
+    scene.add(mesh)
+  })
+}
 
 function flatten(lss) {
   return [].concat(...lss)
@@ -44,7 +51,9 @@ const dlight = new three.DirectionalLight( 0xffffff, 0.7 );
 dlight.position.set( 0.1, 1, 1 );
 scene.add( dlight );
 
-// TODO: watch file
+load()
+fs.watch(nw.App.argv[0], load)
+
 // TODO: load color from binary stl if possible
 // TODO: move camera to fit bounding box (THREE has a bounding box function)
 // TODO: center target by checking bounding box
